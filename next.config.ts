b75d852f2374ea45da524073a84a8abdfc4d7258
joangoma-code <joangomara93@gmail.com>
2026-07-13 21:02:27 +0200

    mountains branch creation

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  allowedDevOrigins: ['192.168.1.110',],
+  allowedDevOrigins: ['192.168.1.110','192.168.1.48'],
 };
 
 export default nextConfig;
