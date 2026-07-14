37cef5b350437fa0b4fb16ece33b1ebe2255ca44
joangoma-code <joangomara93@gmail.com>
2026-07-14 21:03:20 +0200

    mountain.svg test

--- components/sections/HomeSection.tsx
+++ components/sections/HomeSection.tsx
@@ -2,44 +2,44 @@
 
 import Button from "@/components/ui/Button";
 import Container from "@/components/ui/Container";
+import SvgComponent from "../layout/mountains/MountainBack";
 
 export default function HomeSection() {
   return (
-    <>
-      <section
-        id="home"
-        className="flex min-h-screen items-center justify-center bg-linear-to-t from-(--color-background4) to-(--color-background5)"
-      >
-        <Container>
-          <div className="max-w-3xl">
-            <p className="mb-4 text-sm uppercase tracking-[0.3em] opacity-70">
-              Frontend Developer · React · Next.js · TypeScript
-            </p>
+    <section
+      id="home"
+      className=" relative flex min-h-dvh items-center justify-center bg-linear-to-t from-(--color-background4) to-(--color-background5)"
+    >
+      <Container>
+        <div className="max-w-3xl">
+          <p className="mb-4 text-sm uppercase tracking-[0.3em] opacity-70">
+            Frontend Developer · React · Next.js · TypeScript
+          </p>
 
-            <h1 className="text-5xl font-bold leading-tight md:text-7xl">
-              Joan Goma
-            </h1>
+          <h1 className="text-5xl font-bold leading-tight md:text-7xl">
+            Joan Goma
+          </h1>
 
-            <p className="mt-6 max-w-prose text-lg leading-relaxed opacity-80">
-              Focused on building simple, fast, and thoughtful web experiences.
-              I enjoy turning ideas into real products through code.
-            </p>
-            <p className="mt-4 text-sm opacity-60">
-              Open to Frontend Developer opportunities.
-            </p>
+          <p className="mt-6 max-w-prose text-lg leading-relaxed opacity-80">
+            Focused on building simple, fast, and thoughtful web experiences. I
+            enjoy turning ideas into real products through code.
+          </p>
+          <p className="mt-4 text-sm opacity-60">
+            Open to Frontend Developer opportunities.
+          </p>
 
-            <div className="section-content mt-10 flex gap-4">
-              <Button href="#projects">View Projects</Button>
+          <div className="section-content mt-10 flex gap-4">
+            <Button href="#projects">View Projects</Button>
 
-              <Button href="#contact" variant="secondary">
-                Get in touch
-              </Button>
-            </div>
+            <Button href="#contact" variant="secondary">
+              Get in touch
+            </Button>
           </div>
-        </Container>
-      </section>
-      <div className=" w-full h-10 bg-(--color-background3)"></div>
-      <div className=" w-full h-10 bg-(--color-background2)"></div>
-    </>
+        </div>
+      </Container>
+      <div className="absolute inset-x-0 bottom-0 w-full pointer-events-none">
+        <SvgComponent className="absolute bottom-0 left-0 w-full z-10" />
+      </div>
+    </section>
   );
 }
