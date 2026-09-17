b45726a5ce6f62b499cf0f84c23fba9e82aee027
joangoma-code <joangomara93@gmail.com>
2026-09-17 19:33:38 +0200

    footer information

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,13 +1,15 @@
 import MountainFooter from "@/components/ui/mountains/mountainFooter";
 
 export default function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="relative w-full">
       <MountainFooter className="relative -bottom-1" />
       <div className=" relative bg-(--color-card) pb-8 pt-4 md:pt-0">
-        <div className="mx-auto flex max-w-7xl flex-col items-center text-center justify-between gap-4 px-6 text-sm md:flex-row ">
-          <p>Created with Next.js, React & Tailwind CSS.</p>
-          <p>Developed on Windows & Linux with Git & GitHub.</p>
+        <div className="mx-auto flex max-w-6xl flex-col items-center text-center justify-between gap-4 px-14 text-sm md:flex-row ">
+          <p>© {year} Joan Goma. Built with care.</p>
+          <p>Crafted with Next.js · Deployed on Vercel</p>
         </div>
       </div>
     </footer>
